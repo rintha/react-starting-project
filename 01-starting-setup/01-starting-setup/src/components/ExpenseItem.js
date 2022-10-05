ba00664e7013fd5ab04fc89c2a6d0120236fe943
rintha <rintharithu@gmail.com>
2022-10-05 20:51:50 +0530

    locationOfExpenditure

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 406.67;
+    const locationOfExpenditure = 'bank';
+
   return (
-    <div>
-      <div>oct 5 2021</div>
-      <div>
-        <h2>Food</h2>
-        <div>Rs 10</div>
-      </div>
-      <div>
-        <h2>petrol</h2>
-        <div>Rs 100</div>
-      </div>
-      <div>
-        <h2>Movies</h2>
-        <div>Rs 200</div>
+    <div className='expense-item'>
+      <div>{locationOfExpenditure}-</div>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
