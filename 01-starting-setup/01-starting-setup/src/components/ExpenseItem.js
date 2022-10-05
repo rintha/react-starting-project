87a43915ee8559d3ac09912fb2be98c1fded828d
rintha <rintharithu@gmail.com>
2022-10-05 19:49:23 +0530

    getStartedExpense

---
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>oct 5 2021</div>
+      <div>
+        <h2>Food</h2>
+        <div>Rs 10</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
