3d1260a5a5b18f63504964c48c72f435dfeff57f
rintha <rintharithu@gmail.com>
2022-10-05 19:58:21 +0530

    ThreeExpenseAdded

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -6,6 +6,14 @@ function ExpenseItem() {
         <h2>Food</h2>
         <div>Rs 10</div>
       </div>
+      <div>
+        <h2>petrol</h2>
+        <div>Rs 100</div>
+      </div>
+      <div>
+        <h2>Movies</h2>
+        <div>Rs 200</div>
+      </div>
     </div>
   );
 }
