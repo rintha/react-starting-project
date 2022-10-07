5c248fa822cbf815cc159745cdc095065496266f
rintha <rintharithu@gmail.com>
2022-10-07 12:37:42 +0530

    childrenprops

--- 01-starting-setup/01-starting-setup/src/App.js
+++ 01-starting-setup/01-starting-setup/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -8,12 +8,7 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
       title: "Car Insurance",
@@ -26,26 +21,12 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-    {
-      id: "e5",
-      title: "Vacation",
-      amount: 500,
-      date: new Date(2021, 2, 2),
-    },
   ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      {expenses.map((expenses) => {
-        return (
-          <ExpenseItem
-            title={expenses.title}
-            amount={expenses.amount}
-            date={expenses.date}
-            key={expenses.id}
-          ></ExpenseItem>
-        );
-      })}
+      <Expenses items={expenses} />
     </div>
   );
 }
--- 01-starting-setup/01-starting-setup/src/components/ExpenseDate.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseDate.js
@@ -1,8 +1,8 @@
 import "./ExpenseDate.css";
 
 function ExpenseDate(props) {
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
 
   return (
@@ -13,4 +13,5 @@ function ExpenseDate(props) {
     </div>
   );
 }
+
 export default ExpenseDate;
