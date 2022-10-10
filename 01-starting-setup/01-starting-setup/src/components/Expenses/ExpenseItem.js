e9e742151f1b5143e043fcedb4f694421dea97ce
rintha <rintharithu@gmail.com>
2022-10-10 16:13:05 +0530

    deleteExpense

--- 01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
+function deleteExpenseHandler (){
+
+}
 const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
@@ -11,6 +14,9 @@ const ExpenseItem = (props) => {
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <div className="del-expense">
+      <button onClick={deleteExpenseHandler}>Delete Expense</button>
       </div>
     </Card>
   );
