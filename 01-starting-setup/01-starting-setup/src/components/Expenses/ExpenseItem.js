854cd6a8992a4da661af258e1b30dc9f97598a75
rintha <rintharithu@gmail.com>
2022-10-11 10:18:25 +0530

    useStete

--- 01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
-function deleteExpenseHandler (){
+const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () =>{
+    setTitle('Updated!');
+    console.log(title);
 
 }
-const ExpenseItem = (props) => {
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <div className="del-expense">
-      <button onClick={deleteExpenseHandler}>Delete Expense</button>
+      <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
