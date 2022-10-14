4a0a84ae4f8bcca55bd26e7f3b178363e14e375c
rintha <rintharithu@gmail.com>
2022-10-14 12:52:41 +0530

    removedButton(dumb component)

--- 01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,16 @@
-import React,{useState} from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-
-  const [amount, setAmount] = useState(props.amount);
-
-  const clickHandler = () =>{
-    setAmount('100');
-  
-}
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">₹{amount}</div>
-      </div>
-      <div className="del-expense">
-      <button onClick={clickHandler}>Change expense</button>
+        <div className="expense-item__price">₹{props.amount}</div>
       </div>
     </Card>
   );
